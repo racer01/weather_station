5528a66afc5126176b7d53b4c915bdcac3a3a3e8
racer01 <notisracer@gmail.com>
2016-05-05 23:55:55 +0200

    weather_station base & working display test

---
+++ weather_station/weather_station.ino
@@ -0,0 +1,31 @@
+// weather_station v0.0.1
+// barometer+tempmeter+lcd=fun
+
+#include <SFE_BMP180.h>
+#include <Wire.h>
+
+SFE_BMP180 pressure;
+
+#define ALTITUDE 210.0 //Altitude via GPS - Miskolc, Diósgyőr
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("BOOT");
+  if (!pressure.begin())
+  {
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+  }
+}
+
+void meterWrite(double temp, double pressure)
+{
+  //
+  //lcd.print("
+}
+
+void loop() // repeat 5sec (sum of 1+1+1+1+1secs)
+{
+  
+}
