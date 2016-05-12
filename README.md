96372cc987a00baadbd135f485c4b16d903364c1
Székely-Tóth László <notisracer@gmail.com>
2016-05-12 02:15:03 +0200

    v1.0.0
    - added 2 averaging modes to fix fast-updating numbers
    - rewrote & refactored functions
    - added basic menu system
    - added unified sensor reading, a reading now always takes 100 millis
    - rewrote displaying functions now only updates numbers not the whole
    display

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # weather_station
 temp &amp; barometer
+
+roadmap:
+
+==v0.1.0==
+display_test
+
+==v0.5.0==
+first working version with raw data
+
+<<<DONE>>>
+
+==v1.0.0==
+sensor smoothing
+
+==v1.1.0==
+json via serial monitor
--- src/main.cpp
+++ src/main.cpp
@@ -7,64 +7,175 @@
 #include <LiquidCrystal.h>
 #include <math.h>
 
+// MODEs: 0 - running avg, 1 - last CACHESIZE avg
+#define DEFMODE 1
+
 #define LCDWIDTH  16
 #define LCDHEIGHT  2
+#define CACHESIZE 50
+#define DLY 100
+#define ROWCOUNT 2
 
 #define ALTITUDE 210 // Altitude via GPS - Miskolc, Diósgyőr
 
+const String prefix[ROWCOUNT] = {"Temp:", "Atmo:"};
+const String postfix[ROWCOUNT] = {(String)((char)223) + "C ", "hPa"};
+
+const byte sides[ROWCOUNT][2] = {{5, 13}, {5, 13}}; // [ROW][COLUMN] = {{r0, c0}, {r1, c1}}
+
+
 Sodaq_BMP085 sensor;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-const byte halfScreen = LCDWIDTH / 2;
+byte mode = DEFMODE;
 
-int paddingReq(double in, int side)
+
+float temp_avg;
+long atm_avg;
+
+float temp_data[CACHESIZE];
+long atm_data[CACHESIZE];
+byte data_pointer = 0;
+
+
+void Average(float *temp, long *atm)
+{
+	*temp = 0;
+	*atm = 0;
+	for (byte i = 0; i < CACHESIZE; i++)
+	{
+		*temp += temp_data[i];
+		*atm += atm_data[i] / 100;
+	}
+	*temp /= CACHESIZE;
+	*atm /= CACHESIZE;
+	*atm *= 100;
+}
+
+void SensorRead(float *temp, long *atm) // this must be 100 millisecond
+{
+	unsigned long previousMillis = millis();
+	*temp = sensor.readTemperature();
+	*atm = sensor.readPressure(ALTITUDE);
+	unsigned long currentMillis = millis();
+	while (currentMillis - previousMillis <= DLY)
+	{
+		delay(1);
+		currentMillis = millis();
+	}
+}
+
+
+void FirstWrite()
+{
+	lcd.clear();
+	for (size_t i = 0; i < ROWCOUNT; i++)
+	{
+		lcd.setCursor(0, i);
+		lcd.print(prefix[i]);
+		lcd.setCursor(LCDWIDTH - postfix[i].length(), i);
+		lcd.print(postfix[i]);
+	}
+}
+
+int PaddingReq(double in, byte row)
 {
   if(in > 999 || in < -99)
-    return side - 4;
+    return sides[row][1] - 4;
   else if(in > 99 || in < -9)
-    return side - 3;
+    return sides[row][1] - 3;
   else if(in > 9 || in < 0)
-    return side - 2;
+    return sides[row][1] - 2;
   else // 0 < in < 10
-    return side - 1;
+    return sides[row][1] - 1;
 }
 
-void paddedWrite(double in, int decimals, int row, String postfix)
+void PaddedWrite(double in, byte decimals, byte row)
 {
-  byte side = LCDWIDTH - postfix.length();
-  // decimal mark
-  byte dm = 0;  if(decimals > 0) dm = 1;
+	// decimal mark
+	byte dm = 0;  if(decimals > 0) dm = 1;
 
-  lcd.setCursor(paddingReq(in, side) - dm - decimals, row);
-  lcd.print((round(in * pow(10, decimals)) / pow(10, decimals)), decimals);
-  lcd.setCursor(side, row);
-  lcd.print(postfix);
+	// clear row
+	for (byte i = 0; i < (sides[row][1] - sides[row][0]); i++)
+	{
+		lcd.setCursor(sides[row][0], row);
+		lcd.write(' ');
+	}
+	lcd.setCursor(PaddingReq(in, row) - dm - decimals, row);
+	lcd.print((round(in * pow(10, decimals)) / pow(10, decimals)), decimals);
 }
 
-void meterWrite(float temp, double pressure, double decimals)
+void MeterWrite(float temp, double pressure, double decimals)
 {
-  lcd.setCursor(0, 0);
-  lcd.print("Temp:");
-  paddedWrite(temp, decimals, 0, (String)((char)223) + "C ");
-
-  lcd.setCursor(0, 1);
-  lcd.print("Atmo:");
-  paddedWrite(pressure, decimals, 1, "hPa");
+	PaddedWrite(temp, decimals, 0);
+	PaddedWrite(pressure, decimals, 1);
 }
+
+void SimpleInit()
+{
+	temp_avg = sensor.readTemperature();
+	atm_avg = sensor.readPressure(ALTITUDE);
+	SensorRead(&temp_avg, &atm_avg);
+}
+
+void SimpleStep()
+{
+	float temp = 0;
+	long atm = 0;
+	SensorRead(&temp, &atm);
+	temp_avg = (temp_avg + temp) / 2;
+	atm_avg = (atm_avg + atm) / 2;
+}
+
+void AdvancedInit()
+{
+	for (data_pointer = 0; data_pointer < CACHESIZE; data_pointer++)
+	{
+		SensorRead(&temp_data[data_pointer], &atm_data[data_pointer]);
+	}
+	data_pointer = 0;
+}
+
+void AdvancedStep()
+{
+	SensorRead(&temp_data[data_pointer], &atm_data[data_pointer]);
+
+	if (data_pointer < CACHESIZE)
+		data_pointer++;
+	else
+		data_pointer = 0;
+}
+
+void initialize(byte m)
+{
+	switch (m)
+	{
+		case 0: SimpleInit();
+			break;
+		case 1: AdvancedInit();
+			break;
+	}
+}
+
 
 
 void setup()
 {
-  lcd.begin(LCDWIDTH, LCDHEIGHT);
-  lcd.clear();
-  lcd.print("BOOT");
-  sensor.begin();
+	lcd.begin(LCDWIDTH, LCDHEIGHT);
+	lcd.clear();
+	lcd.print("BOOT");
+	sensor.begin();
+	initialize(mode);
+	FirstWrite();
 }
 
-void loop() // repeat 5sec (sum of 1+1+1+1+1secs)
+void loop()
 {
-	double T, p0;
-	T = sensor.readTemperature();
-	p0 = sensor.readPressure(ALTITUDE);
-	meterWrite(T, p0 / 100, 1);
-	delay(100);
+	switch (mode) {
+		case 0: SimpleStep();
+			break;
+		case 1: AdvancedStep();
+				Average(&temp_avg, &atm_avg);
+			break;
+	}
+	MeterWrite(temp_avg, atm_avg / 100, 1);
 }
